dcc82064d24d547bdf51ff5d541ba13d2170690f
Souvik Maji <souvik2015.svk@gmail.com>
2022-11-01 21:38:48 +0530

    Project-Part04_2-Commit_8_Nov012022_Version_1.0

---
+++ onlineshopping-frontend/src/main/java/com/svk/onlineshopping_frontend/controller/jsonDataController.java
@@ -0,0 +1,35 @@
+package com.svk.onlineshopping_frontend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.svk.onlineshopping_backend.dao.ProductDAO;
+import com.svk.onlineshopping_backend.dto.Product;
+
+@Controller
+@RequestMapping("/json/data")
+public class jsonDataController {
+	
+	@Autowired
+	private ProductDAO productDAO;
+	
+	@RequestMapping("/all/products")
+	@ResponseBody
+	public List<Product> getAllProduct()
+	{
+		return productDAO.listActiveProduct();
+	}
+	
+	@RequestMapping("/category/{id}/products")
+	@ResponseBody
+	public List<Product> getProductByCategory(@PathVariable int id)
+	{
+		return productDAO.listActiveProductByCategory(id);
+	}
+
+}
